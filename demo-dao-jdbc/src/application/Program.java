7e2a522d0447c796e51f5986bc6dbbabf160fb63
Nicolas <nlarruda@hotmail.com>
2021-08-12 18:02:45 -0300

    Add seller insert

--- demo-dao-jdbc/src/application/Program.java
+++ demo-dao-jdbc/src/application/Program.java
@@ -1,5 +1,7 @@
 package application;
 
+
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -30,6 +32,16 @@ public class Program {
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
+		
+		
+		System.out.println("\n=== Test 3: seller insert ===");
+		Seller newSeller = new Seller(null, "José Guanabara", "guanabara@mail.com",
+				new Date(), 5000.0, new Department(3, null));
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted! New id = " + newSeller.getId());
+				
+				
+			
 	}
 
 }
